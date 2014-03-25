7427bffb218ff16cd3b2c184bf226a58c051ba2c
swat30 <jacobgodin@gmail.com>
2014-03-25 13:38:40 -0300

    Compat setup

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
-import setuptools
 
-setuptools.setup(
-    setup_requires=['pbr>=0.5.21,<1.0'],
-    pbr=True)
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'bricks_occupant',
+]
+
+setup(
+    name='bricks_occupant',
+    description='Bricks guest agent',
+    long_description='',
+    author='Cloud A',
+    author_email='info@clouda.ca',
+    url='https://bitbucket.org/clouda/bricks-occupant',
+    packages=packages,
+    scripts=['bin/bricks_occupant'],
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
